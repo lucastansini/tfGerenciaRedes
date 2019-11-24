9cc815b1b3e0366be292c2ac6ea4e978ca86f953
Lucas Hagen <lucas_hagen@hotmail.com>
2019-11-24 18:31:56 -0300

    Generating Graph
    Signed-off-by: Lucas Hagen <lucas_hagen@hotmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+""" ============================================================================
+Python version verification
+Required Python version: 3.5
+============================================================================ """
+import sys
+
+if __name__ != '__main__':
+    exit(1)
+
+if sys.version_info.major < 3 or (sys.version_info.major == 3
+                                  and sys.version_info.minor < 5):
+    print("Error: this application requires Python 3.5 or newer")
+    exit(1)
+
+""" ======================================================================== """
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 varnish_miss = 0
 varnish_hit = 0
 
@@ -15,3 +34,17 @@ print("Hits: {}".format(varnish_hit))
 print("Miss: {}".format(varnish_miss))
 print("Pass: {}".format(varnish_pass))
 print("Total Requests: {}".format(varnish_total_req))
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'HIT', 'MISS', 'PASS'
+sizes = [varnish_hit, varnish_miss, varnish_pass]
+explode = (0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+plt.show()
+
+plt.show()
