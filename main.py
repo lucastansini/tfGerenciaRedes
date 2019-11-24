08f90993ce1474e5c0f2c2fcbe55fb9ab2ec0dc8
Lucas Hagen <lucas_hagen@hotmail.com>
2019-11-24 19:54:34 -0300

    Real time implementation
    Signed-off-by: Lucas Hagen <lucas_hagen@hotmail.com>

--- main.py
+++ main.py
@@ -14,45 +14,101 @@ if sys.version_info.major < 3 or (sys.version_info.major == 3
 
 """ ======================================================================== """
 
+import matplotlib.animation as anim
 import matplotlib.pyplot as plt
 import numpy as np
-
-varnish_miss = 0
-varnish_hit = 0
+import subprocess
+from subprocess import Popen, PIPE
 
 full_file = ""
 
-with open('log.txt') as f:
-    full_file = f.read()
+if len(sys.argv) != 2:
+    print("Erro. Correct Usage: python3 main.py <docker_container>")
+    exit(1)
 
-varnish_hit = full_file.count("HIT")
-varnish_miss = full_file.count("MISS")
-varnish_pass = full_file.count("-   VCL_call       PASS\n-   VCL_return     fetch")
-varnish_total_req = full_file.count("<< Request  >>")
+varnish_hit  = 0
+varnish_miss = 0
+varnish_pass = 0
+varnish_total_req = 0
 
-print("Hits: {}".format(varnish_hit))
-print("Miss: {}".format(varnish_miss))
-print("Pass: {}".format(varnish_pass))
-print("Total Requests: {}".format(varnish_total_req))
+first = True
+def draw_graph(line):
+    global full_file
+    global first
+    global varnish_hit
+    global varnish_miss
+    global varnish_pass
+    global varnish_total_req
 
-labels = 'HIT', 'MISS', 'PASS'
-sizes = [varnish_hit, varnish_miss, varnish_pass]
-explode = (0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+    n_varnish_hit  = varnish_hit + line.count("HIT")
+    n_varnish_miss = varnish_miss + line.count("MISS")
+    n_varnish_pass = varnish_pass + line.count("VCL_call       PASS")
+    n_varnish_total_req = varnish_total_req + line.count("<< Request  >>")
 
-labels2 = 'Chached', 'Not Cached'
-sizes2 = [varnish_hit, varnish_miss + varnish_pass]
-explode2 = (0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+    if first or ((varnish_hit != n_varnish_hit or varnish_miss != n_varnish_miss or varnish_pass != n_varnish_pass or varnish_total_req != n_varnish_total_req) and n_varnish_hit + n_varnish_miss + n_varnish_pass == n_varnish_total_req):
+        fig, (ax1, ax2) = plt.subplots(1, 2, num="Varnish Requests")
 
-fig, (ax1, ax2) = plt.subplots(1, 2, num="Varnish Requests")
+        labels = 'HIT', 'MISS', 'PASS'
+        sizes = [varnish_hit, varnish_miss, varnish_pass]
+        explode = (0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
-ax1.set_title("Request Result")
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        labels2 = 'Cached', 'Not Cached'
+        sizes2 = [varnish_hit, varnish_miss + varnish_pass]
+        explode2 = (0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
-ax2.set_title("Requests")
-ax2.pie(sizes2, explode=explode2, labels=labels2, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-plt.show()
+        ax1.set_title("Request Result")
+        ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+        ax2.set_title("Requests")
+        ax2.pie(sizes2, explode=explode2, labels=labels2, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+        if first:
+            print("Hits: {}".format(n_varnish_hit))
+            print("Miss: {}".format(n_varnish_miss))
+            print("Pass: {}".format(n_varnish_pass))
+            print("Total Requests: {}".format(n_varnish_total_req))
+            first = False
+            plt.show(block=False)
+        else:
+            print("\033[F", "\033[F", "\033[F", "\033[F", end='', sep='')
+            print("Hits: {}".format(n_varnish_hit))
+            print("Miss: {}".format(n_varnish_miss))
+            print("Pass: {}".format(n_varnish_pass))
+            print("Total Requests: {}".format(n_varnish_total_req))
+            plt.draw()
+            plt.pause(0.001)
+            plt.clf()
+
+    varnish_hit  = n_varnish_hit
+    varnish_miss = n_varnish_miss
+    varnish_pass = n_varnish_pass
+    varnish_total_req = n_varnish_total_req
+
+def run():
+    global full_file
+
+    cmd = "docker exec -it {} ".format(sys.argv[1])
+
+    proc = Popen(['docker', 'exec', sys.argv[1],
+        'varnishlog', '-i', 'VCL_call,VCL_return,ReqURL,VCL_use,Timestamp,ReqStart,BereqURL'], stdout=PIPE, stderr=PIPE)
+
+    while True:
+        retcode = proc.poll()
+        if retcode is not None: # Process finished.
+            running_procs.remove(proc)
+            break
+        else: # No process is done, wait a bit and check again.
+            print("Monitoring...\n")
+            for line in iter(proc.stdout.readline,''):
+                full_file += line.rstrip().decode('utf8') + '\n'
+                draw_graph(line.rstrip().decode('utf8'))
+
+            time.sleep(1)
+            continue
+
+run()
